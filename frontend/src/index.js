a87402e6913e4d3608fee5ef2d95eb5ce17a8f37
arianna <arianna.n.robinson@gmail.com>
2022-04-01 13:43:09 -0400

    login session reducer and actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,8 @@ import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
+import * as sessionActions from './store/session';
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
@@ -16,10 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
